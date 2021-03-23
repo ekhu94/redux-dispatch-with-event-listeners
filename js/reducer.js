558ec17bd89eeac4227dc1b7eac758b142cfa306
Erik Huang <ekhu94@gmail.com>
2021-03-23 14:51:34 -0700

    commit tree 7b168893ccaa4ced6a8e444dffab55b1c3629ed9

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,27 @@
-// add code snippets from README
+let state;
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+const reducer = (state={count: 0}, action) => {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1};
+    default:
+      return state;
+  }
+}
+
+const dispatch = action => {
+    state = reducer(state, action);
+    render();
+}
+
+const render = () => {
+    const container = document.querySelector('#container');
+    container.innerText = state.count;
+}
+
+dispatch({ type: '@@INIT' });
